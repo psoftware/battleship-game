41a62ee263daba4ccd852f89e72c1545b9570a2e
Antonio Le Caldare <antonio.lecaldare@gmail.com>
2016-11-30 01:11:08 +0100

    connect commands are now non-blocking (multiplexed)

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 rm commlib.o server.o client.o server client
 
-gcc -Wall -c lib/commlib.c -o commlib.o
+gcc -Wall -g -c lib/commlib.c -o commlib.o
 
-gcc -Wall -c server.c -o server.o
+gcc -Wall -g -c server.c -o server.o
 gcc server.o commlib.o -o server
 
-gcc -Wall -c client.c -o client.o
+gcc -Wall -g -c client.c -o client.o
 gcc client.o commlib.o -o client
--- server.c
+++ server.c
@@ -19,7 +19,7 @@
 
 /*** STRUTTURE DATI ***/
 
-enum cl_status {WAIT_INIT, READY, INGAME};
+enum cl_status {WAIT_INIT, CONNECTING, WAIT_CONN_REPLY, READY, INGAME};
 
 typedef struct des_clients
 {
@@ -27,6 +27,8 @@ typedef struct des_clients
 	char username[30];
 	char address[15];
 	char port[5];
+
+	int req_conn_sock;		//in questo campo viene temporaneamente depositato il socket del client in attesa di connettersi
 	enum cl_status status;
 
 	struct des_clients * next;
@@ -139,7 +141,7 @@ void cmd_who(char * str)
 	for(q=client_list; q!=NULL; q=q->next)
 		if(q->status==INGAME || q->status==READY)
 		{
-			printf("%s(%s)\n", q->username, (q->status==INGAME)?"occupato":"libero");
+			printf("%s(%s)\n", q->username, (q->status==READY)?"libero":"occupato");
 			strcat(str, "\t");
 			strcat(str, q->username);
 			strcat(str, "(");
@@ -198,6 +200,10 @@ int cmd_connect(int cl_sock, char * buff, int buff_len, char * res_address, char
 		return -3; //username non trovato
 	if(dest_des->status==INGAME)
 		return -4; //giocatore occupato
+	if(dest_des->status==CONNECTING)
+		return -5; //giocatore si sta connettendo a qualcun'altro
+	if(dest_des->status==WAIT_CONN_REPLY)
+		return -6; //qualcun'altro sta già chiedendo al giocatore di giocare
 
 	//invio comando al client con cui il client vuole giocare
 	char temp_buffer[50];
@@ -206,25 +212,67 @@ int cmd_connect(int cl_sock, char * buff, int buff_len, char * res_address, char
 	params[0]=prefix; params[1]=cl_des->username; params[2]=cl_des->address; params[3]=cl_des->port;
 	int size = pack_eos(temp_buffer, params, 4);
 	int ret = send_variable_string(dest_des->sock, temp_buffer, size);
-
-	//ricevo la risposta dal client
-	ret = recv_variable_string(dest_des->sock, temp_buffer);
 	if(ret == 0 || ret == -1)
-		return -1;
+		return -1;	//send fallita/client disconnesso
 
-	printf("Il client destinatario ha risposto %s\n", temp_buffer);
-	if(!strcmp(temp_buffer, "CONNECTACCEPT"))
+	//memorizzo nel descrittore che cl_client sta effettuando la connect e
+	//che l'eventuale risposta deve essere indirizzata ad esso
+	cl_des->status=CONNECTING;
+	dest_des->status=WAIT_CONN_REPLY;
+	dest_des->req_conn_sock = cl_sock;
+
+	return 1;
+}
+
+//ATTENZIONE: la cmd_reply_connect è la seconda fase della cmd_connect.
+//Il send_buffer non è tra i parametri perchè devo rispondere a req_conn_sock
+//e non al socket che ha mandato la CONNECTIONACCEPTED/DECLINED.
+int cmd_reply_connect(int dest_sock, fd_set * master, int accepted)
+{
+	char temp_buffer[100];
+	int message_lenght;
+
+	des_client * dest_des = des_client_find(client_list, dest_sock);
+	if(!dest_des || dest_des->status!=WAIT_CONN_REPLY)
 	{
-		strcpy(res_address, dest_des->address);
-		strcpy(res_port, dest_des->port);
+		printf("cmd_reply_connect: Messaggio non previsto/Errore generico\n");
+		return -1;
+	}
+
+	des_client * cl_des = des_client_find(client_list, dest_des->req_conn_sock);
+	if(!cl_des) // MANCA UNA CONDIZIONE (client in attesa)
+	{
+		printf("cmd_reply_connect: cl_des non trovato\n");
+		return -1;
+	}
+
+	if(accepted==1)
+	{
+		printf("Il client ha accettato la richiesta!\n");
+
 		cl_des->status=INGAME;
 		dest_des->status=INGAME;
-		return 1;
-	}
-	else if(!strcmp(temp_buffer, "CONNECTDECLINE"))
-		return -5;
 
-	return -1;
+		char prefix[]="CONNECTOK";
+		char * params[3];
+		params[0]=prefix; params[1]=dest_des->address; params[2]=dest_des->port;
+		message_lenght = pack_eos(temp_buffer, params, 3);
+	}
+	else
+	{
+		cl_des->status=READY;
+		dest_des->status=READY;
+		printf("Il client non vuole giocare!\n");
+		strcpy(temp_buffer, "USERNOTACCEPTED");
+		message_lenght=strlen("USERNOTACCEPTED")+1;
+	}
+
+	//la send è fatta su req_conn_sock, cioè il client che ha avviato il comando di connect (precedentemente)
+	int ret = send_variable_string(dest_des->req_conn_sock, temp_buffer, message_lenght);
+	if(ret==0 || ret==-1)
+		remove_client(dest_des->req_conn_sock, master);
+
+	return 1;
 }
 
 /**** MAIN ****/
@@ -343,20 +391,35 @@ int main(int argc, char * argv[])
 								strcpy(send_buffer, "USERNOTREADY");
 								break;
 							case -5:
-								printf("Il client non vuole giocare\n");
-								strcpy(send_buffer, "USERNOTACCEPTED");
+								printf("Il client sta effettuando la connessione ad un altro giocatore\n");
+								strcpy(send_buffer, "USERNOTREADY");
 								break;
-							case 1:		//tutto ok, mando le informazioni necessarie al client!
-								printf("Dati corretti, il client ora è pronto\n");
-								char prefix[]="CONNECTOK";
-								char * params[3];
-								params[0]=prefix; params[1]=res_address; params[2]=res_port;
-								message_lenght = pack_eos(send_buffer, params, 3);
+							case -6:
+								printf("Qualcuno ha già chiesto al client di giocare, anche se la richiesta è in attesa\n");
+								strcpy(send_buffer, "USERNOTREADY");
 								break;
+							case 1:
+								continue;	//la cmd_connect ha fatto la richiesta al client,
+											//ora devo aspettare la sua risposta, ma al client
+											//richiedente non devo mandare niente!
 						}
 					}
-					
-					//printf("Sto inviando %s\n", send_buffer);
+					else if(!strcmp(rec_buffer, "CONNECTACCEPT"))	// mando risposta al client che 
+					{												// aveva fatto precedentemente richiesta (accettato)
+						cmd_reply_connect(i, &master, 1);
+						continue;									// Non è prevista risposta al client
+					}
+					else if(!strcmp(rec_buffer, "CONNECTDECLINE"))	// mando risposta al client che 
+					{												// aveva fatto precedentemente richiesta (rifiutato)
+						cmd_reply_connect(i, &master, 0);
+						continue;									// Non è prevista risposta al client
+					}
+					else
+					{
+						printf("Comando non riconosciuto!\n");
+						strcpy(send_buffer, "GENERICERROR");
+					}
+					printf("Sto inviando %s\n", send_buffer);
 					ret = send_variable_string(i, send_buffer, (message_lenght==0)?strlen(send_buffer)+1:message_lenght);
 					if(ret == 0 || ret == -1) {
 						remove_client(i, &master);
